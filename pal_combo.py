ac5e2ba55e8d07925605f0e0077e7a73f0ecf269
Shayna Anderson-Hill <shaynaalice@gmail.com>
2016-07-26 14:26:25 -0700

    Adding list iterations

--- pal_combo.py
+++ pal_combo.py
@@ -15,18 +15,34 @@ def shuffleACopy(x):
         random.shuffle(b) # shuffle the copy
         return b # return the copy
 
-voices_list = [shuffleACopy(voices) for x in range(syllable_num)]
-print(voices_list)
-voice_dict = dict(enumerate(voices_list)) 
-#voice_dict is made using a shortcut, but should use keys from
-#syllable_dict rather than enumerate
+#create iteration list of voice_dicts
+iter_list = []
 
-voice_dict #dictionary of one iteration
+for i in range(iter_num):
+    voices_list = [shuffleACopy(voices) for x in range(syllable_num)]
+    #voice_dict is made using a shortcut, but should use keys from
+    #syllable_dict rather than enumerate
+    voice_dict = dict(enumerate(voices_list)) #dictionary of one iteration
+    #depending on what you want the keys to be
+#    iter_dict = {value : voice_dict.get(key, None) for key, value in syllable_dict.items()}
+#    iter_list.append(iter_dict)
+    iter_list.append(voice_dict)
+
 
 #experimenting with desired output
 for key, value in voice_dict.items():
     print key, value[0]
 
-#create outer dictionary for each iteration
-for i in range(iter_num):
+pal_iter = list(map(lambda x: x -1, voices))
+
+for i in iter_list:
+    #prints dictionaries within list
+    #print i
+    for key, value in i.iteritems():
+        #prints keys and values in each dictionary (first position in
+        #list)
+        #want position to use pal_iter
+        print key, value[0]
+        
+
 
